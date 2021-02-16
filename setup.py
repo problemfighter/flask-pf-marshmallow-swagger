b822fb0b4dbe7904ee65f9d1fb4b9f5b53b12c9a
hmtmcse <hmtm.cse@gmail.com>
2021-02-16 08:44:11 +0600

    Extension Initial Setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Flask-PF-Marshmallow-Swagger',
+    version='1.0',
+    url='http://example.com/flask-sqlite3/',
+    license='Apache 2.0',
+    author='Touhid Mia',
+    author_email='problemfighter.com@gmail.com',
+    description='This is Problem Fighter Flask Marshmallow Swagger Extension',
+    long_description=__doc__,
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache 2.0 License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
