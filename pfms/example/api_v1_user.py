1089fc4ffda8683be0280348695dd3a95116fe50
hmtmcse <hmtm.cse@gmail.com>
2021-02-19 02:20:22 +0600

    change structure

--- pfms/example/api_v1_user.py
+++ pfms/example/api_v1_user.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 user_blueprint = Blueprint('user_blueprint', __name__, url_prefix="/api/v1/user")
 
 
-@user_blueprint.route("/simple-response", method=["GET"])
+@user_blueprint.route("/simple-response", methods=["GET"])
 def simple_response():
     pass
