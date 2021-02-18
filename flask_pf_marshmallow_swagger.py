9c699a1f575f659027445079e979e3db559dd829
hmtmcse <hmtm.cse@gmail.com>
2021-02-18 13:59:03 +0600

    Swagger url and ui integrated

--- flask_pf_marshmallow_swagger.py
+++ flask_pf_marshmallow_swagger.py
@@ -1,25 +1,25 @@
+from flask import Blueprint, render_template
 
-class PFMarshmallowSwagger:
+class PFMarshmallowSwagger():
+
 
     def __init__(self, app=None):
         self.app = app
+        self.blue_print = Blueprint("PFMarshmallowSwagger", __name__, template_folder="templates", static_folder="pf-marshmallow-swagger")
         if app is not None:
             self.init_app(app)
 
     def init_app(self, app):
-        app.config.setdefault('SQLITE3_DATABASE', ':memory:')
-        app.teardown_appcontext(self.teardown)
-        app.add_url_rule("/pf-swagger-json", "pf-swagger-json", self.swagger_json)
-        app.add_url_rule("/pf-swagger-ui", "pf-swagger-ui", self.swagger_ui)
+        self.blue_print.add_url_rule("/pf-swagger-json", "pf-swagger-json", self.swagger_json)
+        self.blue_print.add_url_rule("/pf-swagger-ui", "pf-swagger-ui", self.swagger_ui)
+        app.register_blueprint(self.blue_print)
 
-    def teardown(self, exception):
-        pass
 
     def swagger_json(self):
         return {"key": "value"}
 
     def swagger_ui(self):
-        return {"key": "value"}
+        return render_template('pf-swagger-ui.html')
 
     def print_me(self):
         print("Bismillah")
