7bd30cf9304c783008d780982c3fe86520210f75
hmtmcse <hmtm.cse@gmail.com>
2021-03-20 07:50:17 +0600

    adjusting crud operation

--- pfms/pfapi/base/pfms_base_schema.py
+++ pfms/pfapi/base/pfms_base_schema.py
@@ -1,5 +1,26 @@
-from marshmallow_sqlalchemy import SQLAlchemySchema
+from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field
+from pf_sqlalchemy.db.orm import BaseModel
 
 
 class PfBaseSchema(SQLAlchemySchema):
     pass
+
+
+class PfDetailBaseSchema(PfBaseSchema):
+    class Meta:
+        model = BaseModel
+
+    id = auto_field()
+    created = auto_field()
+    updated = auto_field()
+    uuid = auto_field()
+
+
+def common_exclude():
+    exclude = ("id", "created", "updated", "uuid")
+    return exclude
+
+
+def update_exclude():
+    exclude = ("created", "updated", "uuid")
+    return exclude
