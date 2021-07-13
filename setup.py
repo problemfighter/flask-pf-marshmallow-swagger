2a8d1a38ee17ff30a2356c480654ef085270198b
hmtmcse <hmtm.cse@gmail.com>
2021-07-13 20:54:56 +0600

    improving system

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flask-pf-marshmallow-swagger',
-    version='1.0',
+    version='1.2',
     url='https://github.com/problemfighter/flask-pf-marshmallow-swagger',
     license='Apache 2.0',
     author='Touhid Mia',
@@ -10,6 +10,7 @@ setup(
     description='This is Problem Fighter Flask Marshmallow Swagger Extension',
     long_description=__doc__,
     packages=find_packages(),
+    package_data={'': ['pfms/pf-marshmallow-swagger/**/*', 'pfms/templates/*']},
     zip_safe=False,
     include_package_data=True,
     platforms='any',
